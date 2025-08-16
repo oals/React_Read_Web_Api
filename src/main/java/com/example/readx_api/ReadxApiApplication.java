f8ab44c0f94ff09a56cdfdb2e6e17b7254a90fc5
OMG <oals666@naver.com>
2025-08-16 18:20:05 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/readx_api/ReadxApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.readx_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadxApiApplication.class, args);
+	}
+
+}
